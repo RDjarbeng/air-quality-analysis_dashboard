561078046ee89e2d7c781db6cdd8a056f640ad83
RDjarbeng <richarddjarbeng@gmail.com>
2025-03-06 15:24:14 +0200

    add Github to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,14 @@ export function Footer() {
     <footer className="mt-8 py-6 border-t">
       <div className="container mx-auto px-4 text-center text-gray-600">
         <p>
-          Developed by Richard Djarbeng. {currentYear}
+          Developed by <a 
+              href="https://github.com/RDjarbeng" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-800 underline decoration-dotted"
+            >
+                Richard Djarbeng. {currentYear}
+                </a>
           {' • '}
           <span>Air quality Data obtained from{' '}
             <a 
