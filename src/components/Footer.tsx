7a05ba568cfd37acaeea4c6d7bc753e01bc8bbca
RDjarbeng <richarddjarbeng@gmail.com>
2025-03-07 21:44:21 +0200

    add direct link to repo, minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,14 +7,26 @@ export function Footer() {
     <footer className="mt-8 py-6 border-t">
       <div className="container mx-auto px-4 text-center text-gray-600">
         <p>
-          Developed by <a 
-              href="https://github.com/RDjarbeng" 
+            Visit this page for <a 
+              href="/about" 
               target="_blank" 
               rel="noopener noreferrer"
               className="text-blue-600 hover:text-blue-800 underline decoration-dotted"
             >
-                Richard Djarbeng. {currentYear}
+                project context 
                 </a>
+                {' • '}
+          Developed by Richard Djarbeng | {currentYear}.
+          {' • '}
+          <a 
+              href="https://github.com/RDjarbeng/air-quality-analysis_dashboard" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-800 underline decoration-dotted"
+            >
+                 View on GitHub  
+                </a>
+                 
           {' • '}
           <span>Air quality Data obtained from{' '}
             <a 
